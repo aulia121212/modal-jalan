6325f63e41c13cea691002059097ee3c42643e19
superlutpi <girdza248@gmail.com>
2023-12-15 15:47:28 +0700

    wlee lagi dulu dah pusing

---
+++ ModalJajan/app/src/main/java/com/example/modaljajan/ui/rekomendasiBudget/RekomendasiBudgetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.modaljajan.ui.rekomendasiBudget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.modaljajan.R
+
+class RekomendasiBudgetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rekomendasi_budget)
+    }
+}
