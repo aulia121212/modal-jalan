98764f262032f18f4ea8996ec4da3a2166b154ba
superlutpi <girdza248@gmail.com>
2023-12-15 15:33:00 +0700

    pada kesempatan kali ini, saya telah menambahkan halaman add pungli

---
+++ ModalJajan/app/src/main/java/com/example/modaljajan/ui/pungli/AddPungliActivity.kt
@@ -0,0 +1,12 @@
+package com.example.modaljajan.ui.pungli
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.modaljajan.R
+
+class AddPungliActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pungli)
+    }
+}
